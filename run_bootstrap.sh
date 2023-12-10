8cdd2d4d77ed0118af850cfba2aa205480246ebb
Shankar Rasiah <srasiah@outlook.com>
2023-12-09 19:35:23 -0600

    code clean up

--- run_bootstrap.sh
+++ run_bootstrap.sh
@@ -15,6 +15,6 @@ NUXEO_NX_STUDIO=$(grep '^NUXEO_NX_STUDIO_PROJECT' ${NUXEO_ENV_SECRET} | tail -n 
 NUXEO_NX_STUDIO_VER=$(grep '^NUXEO_NX_STUDIO_VER' ${NUXEO_ENV_SECRET} | tail -n 1  | cut -d '=' -f2)
 NUXEO_WEBUI=$(grep '^NUXEO_WEBUI' ${NUXEO_ENV_SECRET} | tail -n 1  | cut -d '=' -f2)
 
-rm -rf $NUXEO_NX_STUDIO
+#rm -rf $NUXEO_NX_STUDIO
 
 STUDIO_USERNAME=$NUXEO_STUDIO_USER CREDENTIALS="${NUXEO_STUDIO_PASSWORD}" IMAGE_TYPE=${NUXEO_IMAGE_TYPE} FQDN=$NUXEO_FQDN NX_STUDIO=${NUXEO_NX_STUDIO} NX_STUDIO_VER=${NUXEO_NX_STUDIO_VER} WEBUI=${NUXEO_WEBUI} bash -c  "$(curl -fsSL https://raw.github.com/srasiah/nuxeo-presales-docker/2023-dev-sr/bootstrap.sh)" 
